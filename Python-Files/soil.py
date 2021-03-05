7d213fc4eff1a1a7cd6caa76ac42988cc0617930
Randel M <Randel.C.Moore@gmail.com>
2021-03-05 15:57:29 -0800

    add functionality to get sensor data and convert to percentage

---
+++ Python-Files/soil.py
@@ -0,0 +1,29 @@
+import time
+import Adafruit_ADS1x15
+
+dryValue = 2.23  #Calibration Value with sensor in Air (/10000)
+wetValue = 1.09  #Calibration Value with sensor in Water (/10000)
+
+adc = Adafruit_ADS1x15.ADS1115()
+
+GAIN = 1
+
+def mapToPercent(x,a,b,c,d):
+    if x>a:
+        return 0
+    if x<b:
+        return 100
+    y = (x-a)/(b-a)*(d-c)+c
+    return round(y)
+
+adc.start_adc(0, gain=GAIN)
+
+print("Readout of Soil Moisture Data Begins Now")
+
+while True:
+    value = adc.get_last_result()
+    voltage = value/10000
+    
+    percent = mapToPercent(voltage,dryValue,wetValue,0,100)
+    print("Channel 0: {0}".format(percent))
+    time.sleep(3)
